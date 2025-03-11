a5876a8a486b944fd3d5df5019f9577bcf622333
Moshua53 <moisesjoshua53@gmail.com>
2025-03-11 11:15:28 -0500

    Update todo.py
    Class TodoBook

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -9,8 +9,16 @@ class Todo:
     def mark_completed(self):
         self.completed=True
 
+    def add_tag(self,tag:str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+    def __str__(self)->str:
+        return f"{self.code_id} - {self.title}"
 
+class TodoBook:
+    def __init__(self):
+        self.todos:dict={}
 
-
+    def add_todo(self,title: str,description: str):
 
 
